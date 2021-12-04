d5bef6978d4d81d0da14e9443d2927dee9e968df
Eduardo Lima <carlos.rocha@compasso.com.br>
2021-12-04 11:55:14 -0300

    Criacao de interfaces

---
+++ src/Lab.Business/Interfaces/IEnderecoRepository.cs
@@ -0,0 +1,11 @@
+﻿using Lab.Business.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace Lab.Business.Interfaces
+{
+    public interface IEnderecoRepository : IRepository<Endereco>
+    {
+        Task<Endereco> ObterEnderecoPorFornecedor(Guid fornecedorId);
+    }
+}
---
+++ src/Lab.Business/Interfaces/IProdutoRepository.cs
@@ -0,0 +1,14 @@
+﻿using Lab.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Lab.Business.Interfaces
+{
+    public interface IProdutoRepository : IRepository<Produto>
+    {
+        Task<IEnumerable<Produto>> ObterProdutosPorFornecedor(Guid fornecedorId);
+        Task<IEnumerable<Produto>> ObterProdutosFornecedores();
+        Task<Produto> ObterProdutoFornecedor(Guid id);
+    }
+}
