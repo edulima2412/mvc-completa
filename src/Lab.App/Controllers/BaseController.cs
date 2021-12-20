f8cef057e720c733239150d9713f00b01242547f
Eduardo Lima <carlos.rocha@compasso.com.br>
2021-12-20 08:50:36 -0300

    Personalizando respostas de validacao

--- src/Lab.App/Controllers/BaseController.cs
+++ src/Lab.App/Controllers/BaseController.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Lab.Business.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Lab.App.Controllers
 {
     public abstract class BaseController : Controller
     {
+        private readonly INotificador _notificador;
 
+        protected BaseController(INotificador notificador)
+        {
+            _notificador = notificador;
+        }
+
+        protected bool OperacaoValida()
+        {
+            return !_notificador.TemNotificacao();
+        }
     }
 }
--- src/Lab.App/Controllers/ProdutosController.cs
+++ src/Lab.App/Controllers/ProdutosController.cs
@@ -14,12 +14,18 @@ namespace Lab.App.Controllers
     public class ProdutosController : BaseController
     {
         private readonly IProdutoRepository _produtoRepository;
+        private readonly IProdutoService _produtoService;
         private readonly IFornecedorRepository _fornecedorRepository;
         private readonly IMapper _mapper;
 
-        public ProdutosController(IProdutoRepository produtoRepository, IFornecedorRepository fornecedorRepository, IMapper mapper)
+        public ProdutosController(IProdutoRepository produtoRepository,
+            IProdutoService produtoService,
+            IFornecedorRepository fornecedorRepository,
+            IMapper mapper,
+            INotificador notificador) : base(notificador)
         {
             _produtoRepository = produtoRepository;
+            _produtoService = produtoService;
             _fornecedorRepository = fornecedorRepository;
             _mapper = mapper;
         }
@@ -66,7 +72,9 @@ namespace Lab.App.Controllers
 
             produtoViewModel.Imagem = imgPrefixo + produtoViewModel.ImagemUpload.FileName;
 
-            await _produtoRepository.Adicionar(_mapper.Map<Produto>(produtoViewModel));
+            await _produtoService.Adicionar(_mapper.Map<Produto>(produtoViewModel));
+
+            if (!OperacaoValida()) return View(produtoViewModel);
 
             return RedirectToAction(nameof(Index));          
         }
@@ -111,7 +119,10 @@ namespace Lab.App.Controllers
             produtoAtualizacao.Valor = produtoViewModel.Valor;
             produtoAtualizacao.Ativo = produtoViewModel.Ativo;
 
-            await _produtoRepository.Atualizar(_mapper.Map<Produto>(produtoAtualizacao));
+            await _produtoService.Atualizar(_mapper.Map<Produto>(produtoAtualizacao));
+
+            if (!OperacaoValida()) return View(produtoViewModel);
+
             return RedirectToAction(nameof(Index));
         }
 
@@ -136,7 +147,9 @@ namespace Lab.App.Controllers
 
             if (produtoViewModel == null) return NotFound();
 
-            await _produtoRepository.Remover(id);
+            await _produtoService.Remover(id);
+
+            if (!OperacaoValida()) return View(produtoViewModel);
 
             return RedirectToAction(nameof(Index));
         }
